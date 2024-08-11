f14e68f9e4b12c44a78d5b1028e94c582c1b9abd
Carlos Torres <69270612+carlostorresg@users.noreply.github.com>
2024-08-11 17:49:18 -0600

    updates

--- bank/checking.py
+++ bank/checking.py
@@ -2,7 +2,28 @@ from .account import BankAccount
 
 class CheckingAccount(BankAccount):
 
-    def __init__(self,name,position):
-        super().__init__(name,position)
-        self.position = position 
+    def __init__(self,name):
+        super().__init__(name)
+        
+        
+
+    
+    def withdraw_overdraft(self,amount,overdraft=100):
+
+        
+        if self.balance + overdraft > amount:
+            self.balance -= amount
+            print(f"You withdraw {amount} from your card ")
+            transaction = f'You withdraw {amount} from your card'
+            self.transactions.append(transaction)
+            return self.balance
+
+
+
+carlos = CheckingAccount("carlos")
+carlos.withdraw_overdraft(20)
+        
+
+
+
         
